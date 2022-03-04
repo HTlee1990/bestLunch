b6cb1383de70e49cde32c9faeebb65b7fba492fa
HTlee1990 <notbibu@gmail.com>
2022-03-05 02:49:09 +0900

    Add Tab Effect to modal

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="modal__wrapper" @click.self="closeModal">
     <div class="form__wrapper">
-      <div class="header"><h3>Sign In</h3></div>
-      <div class="header"><h3>Login In</h3></div>
-      <form class="input__wrapper" action="">
+      <div class="header__wrapper">
+        <div
+          class="header"
+          :class="[!isLoginTab ? 'clicked' : '']"
+          @click="handleTab(false)"
+        >
+          <h3>SignIn</h3>
+        </div>
+        <div
+          class="header"
+          :class="[isLoginTab ? 'clicked' : '']"
+          @click="handleTab(true)"
+        >
+          <h3>LogIn</h3>
+        </div>
+      </div>
+      <form class="input__wrapper" action="" v-if="!isLoginTab">
         <div class="label__wrapper">
           <label for="id">id</label>
           <input id="id" type="text" v-model="form__data.id" />
@@ -23,6 +37,23 @@
         </div>
         <button @click.prevent="signInHandler">Sign in</button>
       </form>
+      <form class="input__wrapper" action="" v-if="isLoginTab">
+        <div class="label__wrapper">
+          <label for="id">id</label>
+          <input id="id" type="text" v-model="form__data.id" />
+        </div>
+
+        <div class="label__wrapper">
+          <label for="email">email</label>
+          <input id="email" type="text" v-model="form__data.email" />
+        </div>
+        <div class="label__wrapper">
+          <label for="pw">password</label>
+          <input id="pw" type="text" v-model="form__data.password" />
+        </div>
+
+        <button @click.prevent="signInHandler">Login</button>
+      </form>
     </div>
   </div>
 </template>
@@ -32,7 +63,7 @@ import { signInApi } from "@/api/api.js"
 export default {
   data() {
     return {
-      isLogin: false,
+      isLoginTab: false,
       form__data: {
         id: "",
         email: "",
@@ -42,6 +73,9 @@ export default {
     }
   },
   methods: {
+    handleTab(tab) {
+      this.isLoginTab = tab
+    },
     async signInHandler() {
       try {
         await signInApi(this.form__data)
@@ -59,8 +93,35 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.header {
-  margin: 1rem 0;
+@import "@/style/common";
+
+.header__wrapper {
+  display: flex;
+  .header {
+    margin: 1rem 1rem;
+
+    &.clicked {
+      color: $col-primary;
+    }
+    &.clicked::after {
+      border: 1px solid $col-primary;
+    }
+    cursor: pointer;
+    position: relative;
+    &::after {
+      content: "";
+      border: 1px solid black;
+      position: absolute;
+      width: 100%;
+      bottom: 0px;
+    }
+    &:hover {
+      color: $col-primary;
+    }
+    &:hover::after {
+      border: 1px solid $col-primary;
+    }
+  }
 }
 .modal__wrapper {
   position: absolute;
@@ -92,6 +153,7 @@ export default {
       .label__wrapper {
         display: flex;
         flex-direction: column;
+
         margin: 0 1rem;
       }
     }
